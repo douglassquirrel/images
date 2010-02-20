916b83e88bf021898beb88cb8b138056c86c08a8
douglassquirrel <squirrel@blindpages.com>
2010-02-20 10:45:17 +0000

    screenshot, and turn off auth checking

---
+++ screenshot.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'httpclient' 
+
+system 'scrot /tmp/screenshot.png'
+question = `zenity --entry --text 'What is your question?'`.chomp
+
+
+HTTPClient.post 'http://images.blindpages.com/requests', 
+                 { "request[question]" => question }
+#                { :file => File.new('/home/foo/bar.jpg' }
+
