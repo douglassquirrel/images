7a454890f9b7bb0a80c39b90f9e3fbe1b472107d
douglassquirrel <squirrel@blindpages.com>
2010-02-20 11:36:04 +0000

    screenshot executable

--- screenshot.rb
+++ screenshot.rb
@@ -1,11 +1,13 @@
+#!/usr/bin/ruby
+
 require 'rubygems'
 require 'httpclient' 
 
 system 'scrot /tmp/screenshot.png'
 question = `zenity --entry --text 'What is your question?'`.chomp
 
-
-HTTPClient.post 'http://images.blindpages.com/requests', 
-                 { "request[question]" => question }
-#                { :file => File.new('/home/foo/bar.jpg' }
+result = HTTPClient.post 'http://images.blindpages.com/requests', 
+                         { "request[question]" => question,
+                           "request[image_file]" => File.new('/tmp/screenshot.png') }
+p result
 
